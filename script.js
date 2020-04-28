bd69a07b912577d189ab175dde6de00eba0f19b9
DonatasI <49387040+DonatasI@users.noreply.github.com>
2020-04-28 18:51:14 +0300

    alpha versija

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+function nrnum() {
+	var sum=0;
+	var no=parseInt(document.getElementById("fnr").value);
+	
+	if(no < 0) {
+		document.getElementById("rez").innerHTML = "Negative numbers are not allowed";		
+	}
+	else {
+		while(no > 0) {
+			sum=sum+no%10;
+			no=Math.floor(no/10);
+		}
+		document.getElementById("rez").innerHTML = sum;
+	}
+}
